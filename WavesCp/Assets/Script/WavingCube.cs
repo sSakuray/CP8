73997cb143b54cf8a8bfa0176f198d71b81f09ec
sSakuray <navixplay@mail.ru>
2024-12-16 11:54:01 +0300

    update2

--- WavesCp/Assets/Script/WavingCube.cs
+++ WavesCp/Assets/Script/WavingCube.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class WavingCube : MonoBehaviour
 {
-    private Rigidbody rb;
     private BlockWave wavePlane;
     private Vector3 originalPosition;
     private float waveSpeed = 2f;  
@@ -12,7 +11,6 @@ public class WavingCube : MonoBehaviour
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
         wavePlane = FindObjectOfType<BlockWave>();
         originalPosition = transform.position;
     }
@@ -21,8 +19,8 @@ public class WavingCube : MonoBehaviour
     {
         float surfaceHeight = wavePlane.GetSurfaceHeight(transform.position);
         Vector3 newPosition = new Vector3(originalPosition.x, surfaceHeight, originalPosition.z);
-        rb.MovePosition(Vector3.Lerp(transform.position, newPosition, Time.deltaTime * waveSpeed));
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * waveSpeed);
         float sway = Mathf.Sin(Time.time * waveSpeed) * 0.1f;
-        rb.MovePosition(new Vector3(newPosition.x + sway, newPosition.y, newPosition.z));
+        transform.position = new Vector3(newPosition.x + sway, newPosition.y, newPosition.z);
     }
 }
